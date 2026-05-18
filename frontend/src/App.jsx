c02bc23a3c5b2fbef4e918fc58d65f5e024e32be
NguyenKhoi <khoimapu2k5@gmail.com>
2026-05-19 00:00:46 +0700

    [SETUP] base code for react jsx and springboot

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
