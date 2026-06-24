ec5afe204a380aeed4f8d8ef80d16f7a83d5e041
lequochoi1910 <lequochoi7390@gmail.com>
2026-06-24 21:36:49 +0700

    [REFACTER] Change CloudinaryService to MediaStorageService

--- backend/src/main/java/com/philosophy/rag/utils/service/MediaStorageService.java
+++ backend/src/main/java/com/philosophy/rag/utils/service/MediaStorageService.java
@@ -1,11 +1,13 @@
 package com.philosophy.rag.utils.service;
 
 import com.philosophy.rag.base.exception.ApiException;
-import com.philosophy.rag.utils.dto.CloudinaryUploadResponse;
+import com.philosophy.rag.utils.dto.UploadResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface CloudinaryService {
-    CloudinaryUploadResponse uploadImage(MultipartFile file) throws ApiException;
-    CloudinaryUploadResponse uploadImage(MultipartFile file, String folder) throws ApiException;
+    UploadResponse uploadImage(MultipartFile file) throws ApiException;
+    UploadResponse uploadImage(MultipartFile file, String folder) throws ApiException;
     void deleteImage(String publicId) throws ApiException;
 }
+
+
